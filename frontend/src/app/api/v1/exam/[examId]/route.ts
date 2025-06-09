befe8b2e73fab6d27a5cf73d8100692bf8800c80
franciscocostalap <franciscocosta.py@gmail.com>
2025-06-09 17:52:12 +0100

    Route fix

--- frontend/src/app/api/v1/exam/[examId]/route.ts
+++ frontend/src/app/api/v1/exam/[examId]/route.ts
@@ -4,16 +4,18 @@ import { NextRequest, NextResponse } from "next/server";
 // GET /api/v1/exam/[examId]
 export async function GET(
   request: NextRequest,
-  { params }: { params: { examId: string } }
+  { params }: { params: Promise<{ examId: string }> }
 ) {
-  
-    const examJson = await getExamById(params.examId);
-    if (!examJson) {
-        return NextResponse.json(
-            { error: "Exam not found." },
-            { status: 404 }
-        );
-    }
-
-    return NextResponse.json(examJson, { status: 200 });
+  const {examId} = await params;
+  const examJson = await getExamById(examId);
+  if (!examJson) {
+    return NextResponse.json(
+      { error: "Exam not found." },
+      { status: 404 }
+    );
+  }
+  return NextResponse.json(examJson, { status: 200 });
 }
+
+
+
